27226d9fd71476519e7de53b6f17195613ba39fd
ceworley <ceworley@bhcc.mass.edu>
2021-11-08 00:29:33 -0500

    it's alive!

---
+++ training/make_exam2_practice.r
@@ -0,0 +1,22 @@
+library("exams")
+
+set.seed(123)
+
+folder = "02_probability/02_exam/"
+n = 50
+outdir = "moo_exam_02"
+files = list.files(folder,pattern=".Rmd")
+name = "exam02_practice"
+fn2 = paste0(outdir,"/",name,".xml")
+exams2moodle(paste0(folder,files),n=n,dir=outdir,converter="pandoc-mathml",
+             name=name)
+tx  <- readLines(fn2)
+tx  <- gsub(pattern = "𝐱", replace = "x", x = tx)
+tx  <- gsub(pattern = "𝐰", replace = "w", x = tx)
+tx  <- gsub(pattern = "𝙰𝙽𝙳", replace = "AND", x = tx)
+tx  <- gsub(pattern = "𝙽𝙾𝚃", replace = "NOT", x = tx)
+tx  <- gsub(pattern = "𝙾𝚁", replace = "OR", x = tx)
+writeLines(tx, con=fn2)
+
+
+
